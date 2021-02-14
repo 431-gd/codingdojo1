868917ff9471f5e35ac2f14e295b5aff51587e92
431-gd <78299822+431-gd@users.noreply.github.com>
2021-02-14 18:30:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 # from flask_cors import CORS
 from blockchain import Blockchain
 from argparse import ArgumentParser
+import requests
 
 app = Flask(__name__)
 # CORS(app)
@@ -18,18 +19,70 @@ def chian():
 
 @app.route('/mine', methods=['POST'])
 def mine():
-    """ mine a block """
-    pass
+   block = test.addBlcok()
+    if block != None:
+        dictBlock = block.__dict__.copy()
+        dictBlock['transactions'] = [tx.__dict__ for tx in dictBlock['transactions']]
+        res = {
+            'Block': dictBlock,
+        }
+        return jsonify(res), 201
+    else:
+        res = {
+            'Error': 'Not able to mine a block',
+        }
+        return jsonify(res), 500 
 
 @app.route('/opentxs', methods=['GET'])
 def opentxs():
-    """ get the unconfirmed transactions or any transaction has not been included in a block """
-    pass
+   txs = test.unconfirmed
+    if txs != None:
+        dictTxs = [tx.__dict__ for tx in txs]
+        res = {
+            'transactions': dictTxs
+        }
+        return jsonify(res), 200
+    else:
+        res = {
+            'Error': 'There is no a transaction',
+        }
+        return jsonify(res), 500 
 
 @app.route('/sendtx', methods=['POST'])
 def sendtx():
-    """ send a transaction"""
-    pass
+  values = request.get_json()
+    if not values:
+        res = {
+            'message': 'There is no input'
+        }
+        return jsonify(res), 400
+
+    required = ['sender', 'receiver', 'amount']
+
+    if not all(key in values for key in required):
+        res = {
+            'Message': 'missing a value'
+        }
+        return jsonify(res), 400
+
+    sender = values['sender']
+    receiver = values['receiver']
+    amount = values['amount']
+    passedTransaction = test.addTransaction(sender, receiver, amount)
+
+    if passedTransaction != None:
+        res = {'transaction': {
+            'sender':  values['sender'],
+            'receiver': values['receiver'],
+            'amount': values['amount']
+            }
+        }
+        return jsonify(res), 200
+    else:
+        res = {
+            'Error': 'The transaction did not pass'
+        }
+        return jsonify(res), 500  
 
 
 if __name__ == '__main__':
